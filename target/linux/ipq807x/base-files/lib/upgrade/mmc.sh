641538a3e8560ec511271f281b25cb181bd09cff
marked <marked@users.noreply.github.com>
2023-04-21 07:45:13 -0500

    Update mmc.sh
    Update mmc.sh to be resilient in environments without losetup.  This occurs yet has been needed during initial install for some platforms.
    
    
    Signed-off-by: marked <marked@users.noreply.github.com>

--- target/linux/ipq807x/base-files/lib/upgrade/mmc.sh
+++ target/linux/ipq807x/base-files/lib/upgrade/mmc.sh
@@ -34,9 +34,11 @@ mmc_do_flash() {
 	local kernel=$2
 	local rootfs=$3
 	
-	# keep sure its unbound
-	losetup --detach-all || {
-		echo Failed to detach all loop devices. Skip this try.
+	# make sure its unbound
+	losetup --detach-all
+	mount | grep loop && {
+		echo "Failed to unmount all loop devices. Skip this try."
+		sleep 10
 		reboot -f
 	}
 
@@ -49,31 +51,35 @@ mmc_do_flash() {
 
 	echo "flashing rootfs to ${rootfs}"
 	tar xf $tar_file ${board_dir}/root -O >"${rootfs}"
+	
+	if [[ $(type -P "losetup") ]]; then
+		# a padded rootfs is needed for overlay fs creation
+		local offset=$(tar xf $tar_file ${board_dir}/root -O | wc -c)
+		[ $offset -lt 65536 ] && {
+			echo Wrong size for rootfs: $offset
+			sleep 10
+			reboot -f
+		}
 
-	# a padded rootfs is needed for overlay fs creation
-	local offset=$(tar xf $tar_file ${board_dir}/root -O | wc -c)
-	[ $offset -lt 65536 ] && {
-		echo Wrong size for rootfs: $offset
-		sleep 10
-		reboot -f
-	}
+		# Mount loop for rootfs_data
+		local loopdev="$(losetup -f)"
+		losetup -o $offset $loopdev $rootfs || {
+			echo "Failed to mount looped rootfs_data."
+			sleep 10
+			reboot -f
+		}
 
-	# Mount loop for rootfs_data
-	local loopdev="$(losetup -f)"
-	losetup -o $offset $loopdev $rootfs || {
-		echo "Failed to mount looped rootfs_data."
-		sleep 10
-		reboot -f
-	}
-
-	echo "Format new rootfs_data at position ${offset}."
-	mkfs.ext4 -F -L rootfs_data $loopdev
-	mkdir /tmp/new_root
-	mount -t ext4 $loopdev /tmp/new_root && {
-		echo "Saving config to rootfs_data at position ${offset}."
-		cp -v "$UPGRADE_BACKUP" "/tmp/new_root/$BACKUP_FILE"
-		umount /tmp/new_root
-	}
+		echo "Format new rootfs_data at position ${offset}."
+		mkfs.ext4 -F -L rootfs_data $loopdev
+		mkdir /tmp/new_root
+		mount -t ext4 $loopdev /tmp/new_root && {
+			echo "Saving config to rootfs_data at position ${offset}."
+			cp -v "$UPGRADE_BACKUP" "/tmp/new_root/$BACKUP_FILE"
+			umount /tmp/new_root
+		}
+	else
+		echo "Warning: missing losetup.  Skipping backup to rootfs_data"
+	fi
 
 	# Cleanup
 	losetup -d $loopdev >/dev/null 2>&1
